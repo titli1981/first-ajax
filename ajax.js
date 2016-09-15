57868b1f89ba340f64afba5b2255c27bd6076fc9
titli1981 <madhumishra084@gmailcom>
2016-09-15 17:32:20 -0400

    Used Information in the Response

--- ajax.js
+++ ajax.js
@@ -4,13 +4,28 @@
 
 $(document).ready(function() {
 
- 
- $("button").click(function(){
+
+ $("#ajax-root").click(function(){
      $.ajax({
          url: 'http://first-ajax-api.herokuapp.com/',
          method: 'GET' ,
          data: {} ,
          dataType: 'text'
+     });
+
+ });
+
+
+ $("#ajax-ping-pong").click(function(){
+     $.ajax({
+         url: 'http://first-ajax-api.herokuapp.com/ping',
+         method: 'GET' ,
+         data: {} ,
+         dataType: 'text'
+     }).done(function(responseData){
+         console.log(responseData);
+         $("#step3456").append(responseData);
+
      });
 
  });
