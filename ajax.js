eabe0ba4efc5f1d3879c0e45da5f8884e897547e
titli1981 <madhumishra084@gmailcom>
2016-09-15 16:26:15 -0400

    completed first Ajax request

--- ajax.js
+++ ajax.js
@@ -1,5 +1,10 @@
 $(document).ready(function () {
-
+ $.ajax({
+     url: 'http://first-ajax-api.herokuapp.com/',
+     method: 'GET' ,
+     data: {} ,
+     dataType: 'text'
+ });
   /* Your code goes here */
 
 });
