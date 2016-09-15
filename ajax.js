01b1bf6d2659ea7f48c7f30fd325ee0e3cac995c
titli1981 <madhumishra084@gmailcom>
2016-09-15 17:05:08 -0400

    Binding to A click Event

--- ajax.js
+++ ajax.js
@@ -1,9 +1,18 @@
-$(document).ready(function () {
- $.ajax({
-     url: 'http://first-ajax-api.herokuapp.com/',
-     method: 'GET' ,
-     data: {} ,
-     dataType: 'text'
+
+
+
+
+$(document).ready(function() {
+
+ 
+ $("button").click(function(){
+     $.ajax({
+         url: 'http://first-ajax-api.herokuapp.com/',
+         method: 'GET' ,
+         data: {} ,
+         dataType: 'text'
+     });
+
  });
   /* Your code goes here */
 
